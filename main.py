079bc0d81235bbf5b22dc8204b00568b2b3c6883
Hemant Choudhary <globalgrowth07@gmail.com>
2022-01-18 06:22:03 +0000

    now can start new calculations

--- main.py
+++ main.py
@@ -20,20 +20,22 @@ operations = {
   "/": divide
 }
 
-num1 = int(input("What is the first number?: "))
-for symbols in operations:
-  print(symbols)
-should_continue = True
+def calculator():
+  num1 = int(input("What is the first number?: "))
+  for symbols in operations:
+    print(symbols)
+  should_continue = True
 
-while should_continue:
-  operation_symbol = input("Pick an operation from the line above: ")
-  num2 = int(input("What is the another number?: "))
-  calculation_function = operations[operation_symbol]
-  answer = calculation_function(num1, num2)
+  while should_continue:
+    operation_symbol = input("Pick an operation from the line above: ")
+    num2 = int(input("What is the another number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
 
-  print(f"{num1} {operation_symbol} {num2} = {answer}") 
+    print(f"{num1} {operation_symbol} {num2} = {answer}") 
 
-  if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit ") == "y":
-    num1 = answer
-  else:
-    should_continue = False
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+      num1 = answer
+    else:
+      should_continue = False
+calculator()
